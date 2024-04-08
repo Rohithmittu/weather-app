af105ba6b698a4e9655c26b55c81dd6d4a2c4dc7
Rohithmittu <103620418+Rohithmittu@users.noreply.github.com>
2024-04-08 19:15:25 +0530

    completed weather app using react js

---
+++ src/components/search/index.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Search = ({search, setSearch ,handleSearch}) => {
+
+
+
+
+
+
+  return (
+    <div className="search-engine">
+      <input
+        type="text"
+        className="city-search"
+        placeholder="Enter City Name"
+        name="search"
+        value={search}
+        onChange={(event) => setSearch(event.target.value)}
+      />
+      <button  onClick={handleSearch}>Search</button>
+    </div>
+  );
+};
+
+export default Search;
